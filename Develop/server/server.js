67732295fb997f4f6ae7319bbc85bf776dc5e592
jacob <jacob.7050@gmail.com>
2023-05-18 15:59:02 -0700

    apollo server added to the server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
+
 const db = require('./config/connection');
 const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+})
 
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({app});
+}
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -19,3 +31,5 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+startApolloServer();
